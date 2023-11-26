7da93f0ec78e27c1482a123d388fcaa433901fcd
Mario Brusarosco <mariobrusarosco@hotmail.com>
2023-11-26 10:54:43 -0300

    feat(App): Adding a google font on Next JS

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,20 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Quicksand } from "next/font/google"
 import "./globals.css";
 import { AnimatedContainerThreeLayers } from "./domain/shared/components/animated-container-three-layers";
 import { AnimatedContainerTwoLayers } from "./domain/shared/components/animated-container-two-layers";
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Mario Brusarosco",
   description: "Mario Brusarosco - Front End Developer",
 };
+
+//TODO [Project]
+// Move this configuration and document it
+// Font Settings
+const quicksand = Quicksand({ subsets: ["latin"], weight: ["300", "400", "700"], variable: "--quicksand" });
+
+console.log({ quicksand })
 
 export default function RootLayout({
   children,
@@ -17,7 +23,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${quicksand.className} ${quicksand.variable}`}>
         <AnimatedContainerTwoLayers>{children}</AnimatedContainerTwoLayers>
         {/* <AnimatedContainerThreeLayers>{children}</AnimatedContainerThreeLayers> */}
       </body>
