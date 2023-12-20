20b02b62a3f3e27c1db12465761e1c4ed2976f0c
Mario Brusarosco <mariobrusarosco@hotmail.com>
2023-12-20 13:47:48 -0300

    [LEARNING/TAILWIND] - set app breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [    
-    './app/**/*.{js,ts,jsx,tsx,mdx}', // Note the addition of the `app` directory.
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}", // Note the addition of the `app` directory.
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
 
     // Or if using `src` directory:
-    './src/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      "m-only": { max: "767px" },
+      tablet: { min: "768px" },
+      desktop: { min: "1280px" },
+    },
     extend: {
       fontFamily: {
-        quicksand: ["var(--font-montserrat)", "sans-serif"]
+        quicksand: ["var(--font-montserrat)", "sans-serif"],
       },
-       backgroundImage: {
-        "main": "url('/main-bg.svg')"
+      backgroundImage: {
+        main: "url('/main-bg.svg')",
       },
       colors: {
         "neutral-white": "#FFF",
@@ -22,9 +27,10 @@ module.exports = {
         "primary-base": "#FF6D6D",
         "primary-dark": "#BB2253",
         "secondary-base": "#0D78A0",
-      }
+        "light-gray": "#908F8F",
+        "light-green": "#5BB494",
+      },
     },
   },
   plugins: [],
-}
-
+};
