0fe698be5f24b6de2fc1b25f23d8dc23decb78fd
Mario Brusarosco <mariobrusarosco@hotmail.com>
2024-01-03 18:12:21 -0300

    fix(Home): Responsiveness for tablet

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center h-full tablet:justify-start mobile-lg:justify-start mobile-lg:pt-[20vh]">
-      <div className="max-w-[289px] mobile-lg:max-w-[408px]">
-        <h2 className="text-3xl text-primary-base mb-4 font-extralight mobile-lg:text-5xl">
+    <main className="h-full flex flex-col items-center justify-center mobile-lg:justify-start mobile-lg:pt-[20vh] tablet:pt-[225px] tablet:pl-[80px]">
+      <div className="max-w-[289px] mobile-lg:max-w-[408px] tablet:max-w-none">
+        <h2 className="text-3xl text-primary-base mb-4 font-extralight mobile-lg:text-5xl   tablet:text-8xl">
           Mario Brusarosco
         </h2>
         <h3 className="text-2xl text-primary-white ml-auto w-fit font-thin tablet:text-3xl">
