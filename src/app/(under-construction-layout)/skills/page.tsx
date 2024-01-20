2125eac9734dc58d5be954c87e7b4afb91919ee4
Mario Brusarosco <mariobrusarosco@hotmail.com>
2024-01-20 11:26:40 -0300

    feat(Skills): page heading animation

--- src/app/(under-construction-layout)/skills/page.tsx
+++ src/app/(under-construction-layout)/skills/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { shared } from "@/domain/shared/animations";
 import { motion } from "framer-motion";
 
 export default function Skills() {
@@ -7,16 +8,9 @@ export default function Skills() {
       <div className="pt-[90px] text-center">
         <div className="text-center">
           <motion.h2
-            initial={{ opacity: 0, y: -20 }}
-            animate={{
-              opacity: 1,
-              y: 0,
-              transition: {
-                type: "spring",
-                damping: 10,
-                stiffness: 150,
-              },
-            }}
+            initial="initial"
+            animate="animate"
+            variants={shared.pageHeading}
             className="text-primary-white text-3xl tablet:text-5xl"
           >
             Skills
