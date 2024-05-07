186ee1e394804b91553aea752834f87d53b47ad7
Mario Brusarosco <mariobrusarosco@hotmail.com>
2024-05-07 12:15:04 -0300

    fix(Menu): upgrading animations and removing dead code

--- src/domain/shared/components/app-footer/animations.ts
+++ src/domain/shared/components/app-footer/animations.ts
@@ -2,46 +2,34 @@ import { Variants } from "framer-motion";
 import { init } from "next/dist/compiled/webpack/webpack";
 
 const outerCircle: Variants = {
-  initial: {
+  default: {
+    strokeWidth: 1,
     pathLength: 1,
-    strokeDasharray: 50,
+    stroke: "grey",
+    rotate: 120,
+    transformOrigin: "center",
+    transition: { type: "spring", damping: 10, stiffness: 150 },
   },
   hover: {
-    pathLength: 0.45,
-    transition: {
-      type: "spring",
-      damping: 10,
-      delay: 0.5,
-      // stiffness: 150,
-      // duration: 10,
-    },
+    strokeWidth: 1,
+    pathLength: 0.85,
+    rotate: 120,
+    stroke: "#D60C4E",
+    transformOrigin: "center",
+    transition: { type: "spring", damping: 10, stiffness: 150 },
   },
-  // visible: {
-  //   pathLength: 1,
-  //   opacity: 1,
-  //   transition: {
-  //     type: "spring",
-  //     damping: 30,
-  //     // delay: 0.5,
-  //     // stiffness: 150,
-  //     // duration: 10,
-  //   },
-  // },
-  // hidden: { pathLength: 0, opacity: 0 },
 };
 
 const labelAnimation: Variants = {
-  // default: {
-  //   // y: 0,
-  //   // x: 0,
-  //   x: "-50%",
-  //   left: "50%",
-  // },
+  default: {
+    x: "-50%",
+    left: "50%",
+    opacity: 0,
+  },
   hover: {
     visibility: "visible",
     opacity: 1,
-    y: -90,
-    // x: "-50%",
+    y: -60,
     transition: {
       type: "spring",
       //A
@@ -56,28 +44,18 @@ const labelAnimation: Variants = {
 };
 
 const innerCircle: Variants = {
-  // iniatial: {
-  // y: "-50%",
-  // x: "-50%",
-  // },
   default: {
     y: "-50%",
     x: "-50%",
-    // x: "-50%",
   },
   hover: {
-    y: -35,
+    y: -45,
     x: "-50%",
-    // x: "-50%",
     opacity: 1,
     transition: {
       type: "spring",
-      //A
       damping: 10,
       stiffness: 150,
-      // B
-      // damping: 12,
-      // stiffness: 100,
     },
     backgroundColor: "#D60C4E",
   },
--- src/domain/shared/components/app-footer/app-footer.tsx
+++ src/domain/shared/components/app-footer/app-footer.tsx
@@ -38,7 +38,7 @@ const listItem = {
 
 const AppFooter = () => {
   return (
-    <footer className="fixed w-screen bottom-0 bg-white/5 p-6 desktop:px-16 desktop:py-12 desktop:flex desktop:justify-center desktop:gap-x-2 desktop:bg-transparent">
+    <footer className="fixed w-screen bottom-0 bg-white/5 p-6 desktop:px-16 desktop:py-12 desktop:flex desktop:justify-center desktop:gap-x-2 desktop:my-6 desktop:bg-transparent">
       <div className="hidden font-light desktop:flex desktop:items-center desktop:gap-10">
         <span className="font-sans font-semibold text-lg uppercase text-pink-100">
           menu
@@ -65,16 +65,16 @@ const AnimatedLink = (props: { path: string; label: string }) => {
   const { hasHover } = useScreenDetector();
 
   return (
-    <motion.li key={path} className="flex">
-      <Link href={path} className="flex flex-col justify-between items-center">
+    <motion.li key={path} className="flex" variants={listItem}>
+      <Link href={path}>
         <motion.div
+          className="relative flex flex-col justify-between items-center gap-y-1"
           whileHover="hover"
           initial="default"
           animate="default"
-          className="relative"
         >
           <motion.div
-            className="h-[8px] w-[8px] absolute origin-center -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 rounded-full bg-pink-100"
+            className="h-[10px] w-[10px] absolute origin-center -translate-x-1/2 -translate-y-10 top-[20px] left-1/2 rounded-full bg-pink-100"
             variants={menu.innerCircle}
           />
           <motion.svg width="40" height="40" viewBox="0 0 40 40">
@@ -82,18 +82,17 @@ const AnimatedLink = (props: { path: string; label: string }) => {
               d="M1 20C1 9.50659 9.50659 1 20 1C30.4934 1 39 9.50659 39 20C39 30.4934 30.4934 39 20 39C9.50659 39 1 30.4934 1 20Z"
               stroke="#FFD1CA"
               stroke-width="2"
-              fill={"transparent"}
-              variants={outlineVariants}
+              variants={menu.outerCircle}
+              fill="transparent"
               className="origin-center"
             />
           </motion.svg>
 
           <motion.span
-            className="font-sans font-light text-2xl text-pink-100 
+            className="font-sans font-light  text-pink-100 
             desktop:absolute
-            w-max
-            destop:opacity-0 desktop:invisible
-            "
+            desktop:text-2xl
+            desktop:w-max destop:opacity-0 desktop:invisible"
             variants={hasHover ? menu.label : undefined}
           >
             {label}
@@ -104,148 +103,4 @@ const AnimatedLink = (props: { path: string; label: string }) => {
   );
 };
 
-const textVariants = {
-  default: {
-    color: "grey",
-    fontSize: "1em",
-    x: 0,
-  },
-  hover: {
-    color: "#eaeef5",
-    fontSize: "1.2em",
-    x: 20,
-    transition: {
-      duration: 0.3,
-    },
-  },
-};
-
-const outlineVariants = {
-  default: {
-    strokeWidth: 1,
-    pathLength: 1,
-    stroke: "grey",
-    rotate: 120,
-    transformOrigin: "center",
-    transition: { duration: 0.5, ease: "easeOut" },
-  },
-  hover: {
-    strokeWidth: 1,
-    pathLength: 0.85,
-    rotate: 120,
-    stroke: "#BF4D00",
-    transformOrigin: "center",
-    transition: { duration: 0.5, ease: "easeOut" },
-  },
-};
-
-// const AnimatedLink = (props: { path: string; label: string }) => {
-//   const { path, label } = props;
-//   const { hasHover } = useScreenDetector();
-
-//   return (
-//     <motion.li key={path} className="flex">
-//       <Link href={path} className="flex flex-col justify-between items-center">
-//         <motion.div whileHover={"hover"} initial="default" animate="default">
-//           <motion.span
-//             className="font-sans font-light text-2xl text-pink-100"
-//             variants={textVariants}
-//           >
-//             {label}
-//           </motion.span>
-
-//           <motion.svg viewBox="0 0 100 20" xmlns="http://www.w3.org/2000/svg">
-//             <motion.path
-//               strokeDasharray="0 1"
-//               variants={outlineVariants}
-//               strokeWidth="1"
-//               fill="none"
-//               stroke="grey"
-//               d="M 0, 0 H 100 V 20 H 0 Z"
-//             />
-//           </motion.svg>
-
-//           <motion.svg
-//             width="2"
-//             height="53"
-//             viewBox="0 0 2 53"
-//             fill="none"
-//             xmlns="http://www.w3.org/2000/svg"
-//           >
-//             <motion.path
-//               variants={outlineVariants}
-//               d="M1 0V53"
-//               stroke="#FFD1CA"
-//             />
-//           </motion.svg>
-
-//           <motion.svg
-//             width="40"
-//             height="40"
-//             viewBox="0 0 40 40"
-//             fill="none"
-//             xmlns="http://www.w3.org/2000/svg"
-//           >
-//             <motion.path
-//               variants={outlineVariants}
-//               d="M1 20C1 9.50659 9.50659 1 20 1C30.4934 1 39 9.50659 39 20C39 30.4934 30.4934 39 20 39C9.50659 39 1 30.4934 1 20Z"
-//               stroke="#FFD1CA"
-//               stroke-width="2"
-//             />
-//           </motion.svg>
-//         </motion.div>
-//       </Link>
-//     </motion.li>
-//   );
-// };
-
 export { AppFooter };
-
-{
-  /* <motion.svg
-            width="40"
-            height="40"
-            viewBox="0 0 40 40"
-            fill="transparent"
-            className="rotate-[120deg]"
-          >
-            <motion.path
-              d="M1 20C1 9.50659 9.50659 1 20 1C30.4934 1 39 9.50659 39 20C39 30.4934 30.4934 39 20 39C9.50659 39 1 30.4934 1 20Z"
-              stroke="#FFD1CA"
-              stroke-width="2"
-              initial="initial"
-              variants={menu.circle}
-              whileHover={{ pathLength: 0.85, strokeDasharray: 50 }}
-            />
-            <motion.div
-              className="h-[8px] w-[8px] rounded-full bg-pink-100"
-              variants={desktopFooter.stem}
-            /> */
-}
-
-{
-  /* <motion.circle
-              cx="20"
-              cy="20"
-              r="20"
-              stroke="#FFD1CA"
-              initial="eita"
-              variants={desktopFooter.circle}
-              whileHover={{ pathLength: 0.85, rotate: -60 }}
-            /> */
-}
-{
-  /* </motion.svg> */
-}
-
-{
-  /* <motion.div
-          className="h-[36px] w-[36px] bg-white-100 rounded-full border border-pink-100 flex items-center justify-center"
-          variants={hasHover ? desktopFooter.circle : undefined}
-        >
-          <motion.div
-            className="h-[8px] w-[8px] rounded-full bg-pink-100"
-            variants={hasHover ? desktopFooter.stem : undefined}
-          />
-        </motion.div> */
-}
