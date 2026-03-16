b1b4da3da29f4babd2f890a964c237e135d5fbe5
Mario Brusarosco <mariobrusarosco@hotmail.com>
2026-03-16 11:05:01 -0300

    refactor: Update experience details layout to include role information and enhance calendar component styling with a new label prop.

--- src/domains/experience/components/experience-location-and-data.tsx
+++ src/domains/experience/components/experience-location-and-data.tsx
@@ -13,47 +13,43 @@ export const ExperienceLocationAndDate = ({
 		>
 			<h3
 				data-ui="experience-selected-title"
-				className="text-lg font-bold font-display bg-background uppercase text-white px-3 py-2 pr-12 w-[180px]"
+				className="text-xl font-bold font-display bg-background uppercase text-white px-3 py-4 pr-12 w-[180px]"
 			>
 				{selectedExperience.company}
 			</h3>
 
-			<div data-ui="experience-role-location-and-date" className="flex gap-4">
-				<div className="grid">
+			<div
+				data-ui="experience-role-location-and-date"
+				className="flex gap-4 justify-around"
+			>
+				<div className="grid place-content-center">
 					<span className="text-background font-bold font-display uppercase">
 						{selectedExperience.mode} from
 					</span>
-					<span className="text-background font-light font-body">
+					<span className="text-background  text-xl font-light font-body">
 						{selectedExperience.location}
 					</span>
 				</div>
 
-				<hr className="w-[1px] h-full border-l border-background/20 dashed" />
+				<hr className="w-[1px] h-14 border-1 border-dashed border-background/10" />
 
-				<div className="grid">
-					<span className="text-background font-bold font-display uppercase">
-						{selectedExperience.mode}
+				<div className="grid place-content-center">
+					<span className="text-background  font-bold font-display uppercase ">
+						role
 					</span>
-					<span className="text-background font-light font-body">
-						{selectedExperience.location}
+					<span className="text-background text-xl font-light font-body">
+						{selectedExperience.role}
 					</span>
 				</div>
 
-				<hr className="w-[1px] h-full border-l border-background/20 dashed" />
+				<hr className="w-[0.5px] h-14 border-1 border-dashed border-background/10" />
 
 				<div className="flex gap-4">
-					<div className="grid">
-						<span className="text-background font-bold font-display uppercase">
-							start
-						</span>
-						<ExperienceCalendar date={selectedExperience.startDate} />
-					</div>
-					<div className="grid">
-						<span className="text-background font-bold font-display uppercase">
-							end
-						</span>
-						<ExperienceCalendar date={selectedExperience.endDate} />
-					</div>
+					<ExperienceCalendar
+						label="from"
+						date={selectedExperience.startDate}
+					/>
+					<ExperienceCalendar label="to" date={selectedExperience.endDate} />
 				</div>
 			</div>
 		</div>
